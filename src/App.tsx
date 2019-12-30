16838ba009a3d3389564ef8fe85ce458ac676652
Brandon Anzaldi <brandon@brandonanzaldi.com>
2019-12-29 17:14:21 -0800

    Tweak insights init, remove unused code in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import { searchStreams } from './services/twitch';
-
 import Footer from './components/Footer';
 import Header from './components/Header';
 import SearchPage from './components/SearchPage';
+
+import { init as initInsights } from './services/insights';
+
 import './App.css';
 
 class App extends React.Component {
-  state = {
-    streams: [],
-    total: 0,
-  };
-
   async componentDidMount() {
-    const { streams, _total: total } = await searchStreams('public');
-    // let streams = chunk(results, DISPLAY_COLUMNS);
-    this.setState({
-      streams,
-      total,
-    });
+    initInsights();
   }
 
   render() {
--- src/services/insights.ts
+++ src/services/insights.ts
@@ -1,7 +1,10 @@
 import * as insights from 'insights-js';
 import { INSIGHTS_ID } from '../constants';
 
-insights.init(INSIGHTS_ID);
+export const init = (): void => {
+  insights.init(INSIGHTS_ID);
+  insights.trackPages();
+};
 
 export const track = (payload: insights.TrackEventPayload): void => {
   payload.parameters = Object.assign(payload.parameters, {
