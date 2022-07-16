554918f0f80356716bbaa7cf03fadb73a023987b
Brandon Anzaldi <brandon@brandonanzaldi.com>
2022-07-15 19:19:11 -0700

    So long, and thanks for all the fish (for now)

---
+++ src/pages/ThankYou.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ThankYou: React.FC<{}> = () => {
+    return (
+        <div style={{padding: '20px 10%'}}>
+            <p>Unfortunately, I haven't had the time to devote to this project, nor the passion to really continue much hobby dev work. I know the site's been broken for a while, and I hope that I can bring it back in some form eventually, but for now, if you're looking for an alternative, might I suggest CommanderRoot's <a href="https://twitch-tools.rootonline.de/channel_previews.php" rel="noopener">Twitch stream filter</a>.</p>
+            <p>Thank you to all of you who used and enjoyed the tool. It's been amazing to see the support people have given this.</p>
+            <p>If you're interested in getting in touch about the project, feel free to contact me on <a href="https://twitch.tv/caffeinewriter" target="_blank" rel="noopener">Twitch</a>, <a href="https://twitter.com/caffeinewriter" target="_blank" rel="noopener">Twitter</a>, or via email at caff / caffeinewriter / com (replace the slashes with an @ and a .)</p>
+            <p>For now, so long, and thanks for all the fish.</p>
+            <p>&lt;3 caffeinewriter</p>
+        </div>
+    )
+}
+
+export default ThankYou;
